c1e3afe204a39e549e5003ceb55cf8aff9118ad4
raghavbrocade <raghav.shenoy@broadcom.com>
2018-04-26 17:24:41 +0530

    changed version to 1.0.0
    changed version to 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='pyfos',
-	version='0.9.0',
+	version='1.0.0',
 	description='Brocade FOS Library.',
 	author='Brocade Communications Systems LLC.',
 	author_email='Automation.BSN@broadcom.com',
